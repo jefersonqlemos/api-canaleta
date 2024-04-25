fff227ff67f55c8a808797b07f58ae550c30a7b3
jeferson lemos <jefersonquagliottolemos@hotmail.com>
2024-04-25 00:48:42 -0300

    firt commit

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 import { getErrorMessage } from '../utils/errors.util';
 import { AppDataSource } from "./../database/app-data-source"
-import { User } from "../models/user.model"
+//import { User } from "../models/user.model"
 //import * as userServices from '../services/user.service';
 
 export const loginOne = async (req: Request, res: Response) => {
  try {
    //const foundUser = await userServices.login(req.body);
-   AppDataSource.manager.find(User)
+   //AppDataSource.manager.find(User)
    res.status(200).send('foundUser');
  } catch (error) {
    return res.status(500).send(getErrorMessage(error));
@@ -17,7 +17,7 @@ export const loginOne = async (req: Request, res: Response) => {
 export const registerOne = async (req: Request, res: Response) => {
  try {
    //await userServices.register(req.body);
-   res.status(200).send('Inserted successfully');
+   //res.status(200).send('Inserted successfully');
  } catch (error) {
    return res.status(500).send(getErrorMessage(error));
  }
--- src/database/app-data-source.ts
+++ src/database/app-data-source.ts
@@ -1,12 +1,15 @@
 import { DataSource } from "typeorm"
+import dotenv from 'dotenv'; 
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true
 })
 
 AppDataSource.initialize()
